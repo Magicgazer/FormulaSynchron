edcbedd12a62b704e1bdf516c9d9d5b9769ecfaa
Dlerk <859343844@qq.com>
2022-04-15 23:25:52 +0800

    dev_1

---
+++ lib/blinker/bCtrl.h
@@ -0,0 +1,37 @@
+/**
+ * @file bCtrl.h
+ * @author Dlerk
+ * @brief Blinker控制模块
+ * @version 1.0
+ * @date 2022-04-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <Blinker.h>
+#include <String.h>
+#define BLINKER_WIFI
+
+class bCtrl
+{
+private:
+    string* auth = "***" ;   //双引号里把*换成blinker.app上的key
+    char ssid[10] = "\0\0\0\0\0\0\0\0\0" ;  //链接Wi-Fi或手机热点名称
+    char pswd[10] = "\0\0\0\0\0\0\0\0\0" ;  //Wi-Fi密码
+
+    // 创建Blinker模块对象
+    BlinkerNumber Dist("dist") ;        //距离
+    BlinkerButton Car1("btn-up");       //前进
+    BlinkerButton Car2("btn-down");     //后退
+    BlinkerButton Car3("btn-left");     //左转
+    BlinkerButton Car4("btn-right");    //右转
+    BlinkerSlider Speed("speed") ;      //速度
+    
+public:
+    bCtrl( char SSID[] , char PSWD[] ) ;
+    ~bCtrl() ;
+
+
+};
+
--- main.ino
+++ main.ino
@@ -1,4 +1,22 @@
-//main loop
+/**
+ * @file main.ino
+ * @author Dlerk
+ * @brief 
+ * @version 1.0
+ * @date 2022-04-15
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#define BLINKER_WIFI
+#include <SR04.h>
+
+#define buzzerPin = 1 ;
+#define ledPin = 13 ;
+
+const char* SSID = "" ;
+const char* PSWD = "" ;
 
 void setup()
 {
